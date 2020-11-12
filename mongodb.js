20ec4c49355cec1c53bf4499d1121e33f07a20ce
Andrew Weinstein <andrew@altvia.com>
2020-11-11 19:27:32 -0700

    handle error; log result

--- mongodb.js
+++ mongodb.js
@@ -4,14 +4,27 @@ const MongoClient = mongodb.MongoClient;
 const connectionUrl = 'mongodb://127.0.0.1:27017';
 const dbName = 'task-manager';
 
-MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (err, client) => {
-  if (err) {
-    return console.error('error connecting to database:', err);
-  }
+MongoClient.connect(
+  connectionUrl,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (connError, client) => {
+    if (connError) {
+      return console.error('error connecting to database:', connError);
+    }
 
-  const db = client.db(dbName);
-  db.collection('users').insertOne({
-    name: 'Andrew',
-    age: 29,
-  });
-});
+    const db = client.db(dbName);
+    db.collection('users').insertOne(
+      {
+        name: 'Andrew',
+        age: 29,
+      },
+      (dbError, dbResult) => {
+        if (dbError) {
+          return console.error('error inserting document:', dbError);
+        }
+
+        console.log(dbResult.ops);
+      }
+    );
+  }
+);
