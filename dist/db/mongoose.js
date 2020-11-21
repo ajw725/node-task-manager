7a3a2f0284753508e9f3a17466f64bdbefd2aefe
Andrew Weinstein <andrew@altvia.com>
2020-11-21 16:08:11 -0700

    install express and nodemon; set up index file

--- dist/db/mongoose.js
+++ dist/db/mongoose.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
+const validator_1 = __importDefault(require("validator"));
 const connString = 'mongodb://127.0.0.1:27017/task_manager_api';
 mongoose_1.default.connect(connString, {
     useNewUrlParser: true,
@@ -11,27 +12,67 @@ mongoose_1.default.connect(connString, {
     useCreateIndex: true,
 });
 const userSchema = new mongoose_1.default.Schema({
-    name: { type: String, required: true },
-    age: { type: Number }
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate: [
+            (val) => validator_1.default.isEmail(val),
+            'Invalid email address',
+        ],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        trim: true,
+        validate: [
+            (val) => !val.toLowerCase().includes('password'),
+            'Password cannot contain the string "password"',
+        ],
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate: [(val) => val >= 0, 'Age cannot be negative'],
+    },
 });
 const User = mongoose_1.default.model('User', userSchema);
-const andrew = new User();
-andrew
-    .save()
-    .then((data) => console.log('created user:', data))
-    .catch(err => console.error('failed to save user', err));
+const user = new User({
+    name: ' Bodie ',
+    email: 'BODIE@example.com',
+    password: ' password123',
+    age: 2,
+});
+// user
+//   .save()
+//   .then((data) => console.log('created user:', data))
+//   .catch((err) => console.error('failed to save user:', err.errors));
 // const invalid = new User({ name: 'Andrew', age: 'invalid' });
 // invalid
 //   .save()
 //   .then((data) => console.log('created user:', data))
 //   .catch(err => console.error('failed to save user', err));
-// const taskSchema = new Mongoose.Schema({
-//   description: { type: String },
-//   completed: { type: Boolean }
-// });
-// const Task = Mongoose.model('Task', taskSchema);
-// const firstTask = new Task({ description: 'learn mongoose', completed: true });
-// firstTask
-//   .save()
-//   .then((t) => console.log('created task:', t))
-//   .catch(e => console.error('failed to create task:', e));
+const taskSchema = new mongoose_1.default.Schema({
+    description: {
+        type: String,
+        required: [true, 'You must provide a task description'],
+        trim: true,
+    },
+    completed: {
+        type: Boolean,
+        default: false,
+    },
+});
+const Task = mongoose_1.default.model('Task', taskSchema);
+const firstTask = new Task({ description: '', completed: true });
+firstTask
+    .save()
+    .then((t) => console.log('created task:', t))
+    .catch((e) => console.error('failed to create task:', e));
--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,24 +1,58 @@
 import Mongoose from 'mongoose';
+import validator from 'validator';
 
 const connString = 'mongodb://127.0.0.1:27017/task_manager_api';
 Mongoose.connect(connString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-})
+});
 
 const userSchema = new Mongoose.Schema({
-  name: { type: String, required: true },
-  age: { type: Number }
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate: [
+      (val: string) => validator.isEmail(val),
+      'Invalid email address',
+    ],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    trim: true,
+    validate: [
+      (val: string) => !val.toLowerCase().includes('password'),
+      'Password cannot contain the string "password"',
+    ],
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate: [(val: number) => val >= 0, 'Age cannot be negative'],
+  },
 });
 
 const User = Mongoose.model('User', userSchema);
 
-const andrew = new User({ name: 'Bodie' });
-andrew
-  .save()
-  .then((data) => console.log('created user:', data))
-  .catch(err => console.error('failed to save user', err));
+const user = new User({
+  name: ' Bodie ',
+  email: 'BODIE@example.com',
+  password: ' password123',
+  age: 2,
+});
+// user
+//   .save()
+//   .then((data) => console.log('created user:', data))
+//   .catch((err) => console.error('failed to save user:', err.errors));
 
 // const invalid = new User({ name: 'Andrew', age: 'invalid' });
 // invalid
@@ -26,13 +60,20 @@ andrew
 //   .then((data) => console.log('created user:', data))
 //   .catch(err => console.error('failed to save user', err));
 
-// const taskSchema = new Mongoose.Schema({
-//   description: { type: String },
-//   completed: { type: Boolean }
-// });
-// const Task = Mongoose.model('Task', taskSchema);
-// const firstTask = new Task({ description: 'learn mongoose', completed: true });
-// firstTask
-//   .save()
-//   .then((t) => console.log('created task:', t))
-//   .catch(e => console.error('failed to create task:', e));
+const taskSchema = new Mongoose.Schema({
+  description: {
+    type: String,
+    required: [true, 'You must provide a task description'],
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+const Task = Mongoose.model('Task', taskSchema);
+const firstTask = new Task({ description: '', completed: true });
+firstTask
+  .save()
+  .then((t) => console.log('created task:', t))
+  .catch((e) => console.error('failed to create task:', e));
