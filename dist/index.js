1c99bc4df35810947161b5713b3cd651cbd97003
Andrew Weinstein <andrew@altvia.com>
2020-11-21 17:02:03 -0700

    update users and tasks

--- dist/index.js
+++ dist/index.js
@@ -11,44 +11,135 @@ const app = express_1.default();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 const port = parseInt(process.env.PORT || '3000');
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new user_1.User(req.body);
-    user
-        .save()
-        .then(() => {
+    try {
+        await user.save();
         res.status(201).send(user);
-    })
-        .catch((err) => {
+    }
+    catch (err) {
         res.status(400).send({ error: err });
-    });
+    }
 });
-app.get('/users', (_req, res) => {
-    user_1.User.find()
-        .then((users) => res.status(200).send(users))
-        .catch((err) => res.status(500).send({ error: err }));
+app.get('/users', async (_req, res) => {
+    try {
+        const users = await user_1.User.find();
+        res.status(200).send(users);
+    }
+    catch (err) {
+        res.status(500).send({ error: err });
+    }
 });
-app.get('/users/:id', (req, res) => {
-    user_1.User.findById(req.params.id)
-        .then((user) => {
+app.get('/users/:id', async (req, res) => {
+    try {
+        const user = await user_1.User.findById(req.params.id);
         if (user) {
             res.status(200).send(user);
         }
         else {
-            res.status(404).send({ error: 'User not found' });
+            res
+                .status(404)
+                .send({ error: `User with id ${req.params.id} not found` });
         }
-    })
-        .catch((err) => res.status(500).send({ error: err }));
+    }
+    catch (err) {
+        res.status(500).send({ error: err });
+    }
 });
-app.post('/tasks', (req, res) => {
+app.patch('/users/:id', async (req, res) => {
+    const givenFields = Object.keys(req.body);
+    const allowedFields = ['name', 'email', 'password', 'age'];
+    if (givenFields.length === 0) {
+        res
+            .status(400)
+            .send({ error: 'You must provide at least one field to update.' });
+    }
+    const isValid = givenFields.every((f) => allowedFields.includes(f));
+    if (!isValid) {
+        res.status(400).send({ error: 'Invalid field provided for update' });
+    }
+    try {
+        const user = await user_1.User.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+        if (user) {
+            res.status(200).send(user);
+        }
+        else {
+            res
+                .status(404)
+                .send({ error: `User with id ${req.params.id} not found` });
+        }
+    }
+    catch (err) {
+        res.status(500).send({ error: err });
+    }
+});
+app.patch('/tasks/:id', async (req, res) => {
+    const givenFields = Object.keys(req.body);
+    if (givenFields.length === 0) {
+        res
+            .status(400)
+            .send({ error: 'You must provide at least one field to update.' });
+    }
+    const allowedFields = ['description', 'completed'];
+    const isValid = givenFields.every((f) => allowedFields.includes(f));
+    if (!isValid) {
+        res.status(400).send({ error: 'Invalid field provided for update' });
+    }
+    try {
+        const task = await task_1.Task.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+        if (task) {
+            res.status(200).send(task);
+        }
+        else {
+            res
+                .status(404)
+                .send({ error: `Task with id ${req.params.id} not found` });
+        }
+    }
+    catch (err) {
+        res.status(500).send({ error: err });
+    }
+});
+app.post('/tasks', async (req, res) => {
     const task = new task_1.Task(req.body);
-    task
-        .save()
-        .then(() => {
+    try {
+        await task.save();
         res.status(201).send(task);
-    })
-        .catch((err) => {
+    }
+    catch (err) {
         res.status(400).send({ error: err });
-    });
+    }
+});
+app.get('/tasks', async (_req, res) => {
+    try {
+        const tasks = await task_1.Task.find();
+        res.status(200).send(tasks);
+    }
+    catch (err) {
+        res.status(500).send(err);
+    }
+});
+app.get('/tasks/:id', async (req, res) => {
+    try {
+        const task = await task_1.Task.findById(req.params.id);
+        if (task) {
+            res.status(200).send(task);
+        }
+        else {
+            res
+                .status(404)
+                .send({ error: `Task with id ${req.params.id} not found.` });
+        }
+    }
+    catch (err) {
+        res.status(500).send(err);
+    }
 });
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -8,46 +8,134 @@ app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
 const port = parseInt(process.env.PORT || '3000');
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   const user = new User(req.body);
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch((err) => {
-      res.status(400).send({ error: err });
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (err) {
+    res.status(400).send({ error: err });
+  }
+});
+
+app.get('/users', async (_req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).send(users);
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+});
+
+app.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      res.status(200).send(user);
+    } else {
+      res
+        .status(404)
+        .send({ error: `User with id ${req.params.id} not found` });
+    }
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+});
+
+app.patch('/users/:id', async (req, res) => {
+  const givenFields = Object.keys(req.body);
+  const allowedFields = ['name', 'email', 'password', 'age'];
+  if (givenFields.length === 0) {
+    res
+      .status(400)
+      .send({ error: 'You must provide at least one field to update.' });
+  }
+
+  const isValid = givenFields.every((f) => allowedFields.includes(f));
+  if (!isValid) {
+    res.status(400).send({ error: 'Invalid field provided for update' });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
     });
+    if (user) {
+      res.status(200).send(user);
+    } else {
+      res
+        .status(404)
+        .send({ error: `User with id ${req.params.id} not found` });
+    }
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
 });
 
-app.get('/users', (_req, res) => {
-  User.find()
-    .then((users) => res.status(200).send(users))
-    .catch((err) => res.status(500).send({ error: err }));
+app.patch('/tasks/:id', async (req, res) => {
+  const givenFields = Object.keys(req.body);
+  if (givenFields.length === 0) {
+    res
+      .status(400)
+      .send({ error: 'You must provide at least one field to update.' });
+  }
+
+  const allowedFields = ['description', 'completed'];
+  const isValid = givenFields.every((f) => allowedFields.includes(f));
+  if (!isValid) {
+    res.status(400).send({ error: 'Invalid field provided for update' });
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (task) {
+      res.status(200).send(task);
+    } else {
+      res
+        .status(404)
+        .send({ error: `Task with id ${req.params.id} not found` });
+    }
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
 });
 
-app.get('/users/:id', (req, res) => {
-  User.findById(req.params.id)
-    .then((user) => {
-      if (user) {
-        res.status(200).send(user);
-      } else {
-        res.status(404).send({ error: 'User not found' });
-      }
-    })
-    .catch((err) => res.status(500).send({ error: err }));
-});
-
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
   const task = new Task(req.body);
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task);
-    })
-    .catch((err) => {
-      res.status(400).send({ error: err });
-    });
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (err) {
+    res.status(400).send({ error: err });
+  }
+});
+
+app.get('/tasks', async (_req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).send(tasks);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.get('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (task) {
+      res.status(200).send(task);
+    } else {
+      res
+        .status(404)
+        .send({ error: `Task with id ${req.params.id} not found.` });
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 app.listen(port, () => {
